46a24ac0ad70b4f3cd77bf8425d2dbcbaaeb7de7
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-12-13 17:43:38 +0100

    urls and views

--- forms-level/basicforms/basicapp/views.py
+++ forms-level/basicforms/basicapp/views.py
@@ -6,5 +6,14 @@ def index(request):
     return render(request, 'basicapp/index.html')
 
 def form_name_view(request):
-    form = formName()
+    form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print("Validation Success!!")
+            print("Name: " + (form.cleaned_data['name']))
+            print("Email: " + (form.cleaned_data['email']))
+            print("Text: " + (form.cleaned_data['text']))
     return render(request, 'basicapp/form_page.html', {'form': form})
