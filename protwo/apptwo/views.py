46f0f0d46e808523ccfbf62affc07aa576df5cb1
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-12-14 20:15:47 +0100

    new user model form

--- protwo/apptwo/views.py
+++ protwo/apptwo/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from apptwo.models import User
+from apptwo.forms import NewUserForm
 
 def index(request):
     return render(request, 'apptwo/index.html')
 
 def users(request):
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("ERROR FORM INVALID")
+    return render(request, 'apptwo/users.html', {'form': form})
+
+    '''
     user_list = User.objects.order_by('first_name')
     user_dict = {'users': user_list}
     return render(request, 'apptwo/users.html', context=user_dict)
-
-def help(request):
-    help_dict = {'help_insert':'HELP PAGE'}
-    return render(request, 'apptwo/help.html', context=help_dict)
-# Create your views here.
+    '''
