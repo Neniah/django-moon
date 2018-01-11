85aebecf1ddcf0458949dfb912c61e8cf338d053
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-01-11 13:39:40 +0100

    django url templates

---
+++ django_level_four/four/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/maria/Projects/python/django/django-moon/django_level_four/four/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
